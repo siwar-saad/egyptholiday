da60183c9547d0cf32371c79e45eb06b14c78676
NesrineGhaddab <ghaddabnessrine@gmail.com>
2026-05-03 11:40:52 +0300

    backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./page/Home";
 import Login from "./page/Login";
-
-
+import BasicFacts from "./page/BasicFacts";
+import History from "./page/History";
+import Geography from "./page/Geography";
+import DestinationsInfo from "./page/DestinationsInfo";
+import Activities from "./page/Activities";
+import Food from "./page/Food";
 
 function App() {
   return (
@@ -10,9 +14,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        
-        
-
+        <Route path="/basic-facts" element={<BasicFacts />} />
+        <Route path="/history" element={<History />} />
+        <Route path="/geography" element={<Geography />} />
+        <Route path="/destinations-info" element={<DestinationsInfo />} />
+        <Route path="/activities" element={<Activities />} />
+        <Route path="/food" element={<Food />} />
       </Routes>
     </BrowserRouter>
   );
